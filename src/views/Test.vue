9a44219cc49256e4d47381ba05d65fbc938f30d7
hisomura <hisomura.noreply@gmail.com>
2019-07-03 09:53:47 +0900

    テストページ追加

---
+++ src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Test!</p>
+    <p>{{ testData }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test.vue",
+  data() {
+    return {
+      testData: "test data!"
+    };
+  }
+};
+</script>
+
+<style scoped></style>
