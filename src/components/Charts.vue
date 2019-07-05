8a4037ea9cf3cd7ae9b11615b220fdbd606b4ddc
hisomura <hisomura.noreply@gmail.com>
2019-07-05 18:58:42 +0900

    tmp

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -12,6 +12,12 @@
 import LineChart from './chart/LineChart.vue'
 import { addDays, addHours } from 'date-fns'
 const getRandomInt = () => Math.floor(Math.random() * (50 - 5 + 1)) + 5
+const sliceAndFill = (arr, offset, size) => {
+  const emptySize = size - (arr.length - arr.length)
+  return arr
+    .slice(offset, offset + size)
+    .concat(Array.from({ length: emptySize }))
+}
 export default {
   components: {
     LineChart
@@ -20,19 +26,19 @@ export default {
     return {
       totalLabels: [],
       totalData: [],
-      offset: 100,
+      offset: 0,
       plotNum: 100
     }
   },
   computed: {
     dataCollection() {
       return {
-        labels: this.totalLabels.slice(this.offset, this.offset + this.plotNum),
+        labels: sliceAndFill(this.totalLabels, this.offset + 500, this.plotNum),
         datasets: [
           {
             label: 'Data One',
             backgroundColor: '#f87979',
-            data: this.totalData
+            data: sliceAndFill(this.totalData, this.offset, this.plotNum)
           }
         ]
       }
@@ -46,18 +52,10 @@ export default {
       this.offset += 50
     },
     fillData() {
-      const dateLength = 300
-      this.totalLabels = Array.from({ length: dateLength }, (v, i) => {
-        return addDays(new Date(), i)
+      this.totalLabels = Array.from({ length: 1000 }, (v, i) => {
+        return addDays(new Date(), i - 500)
       })
-      const startDate = addDays(new Date(), 100)
-      this.totalData = Array.from({ length: 400 }, (v, i) => {
-        return {
-          x: addHours(addDays(startDate, i / 4), (i % 4) * 6),
-          y: getRandomInt()
-        }
-      })
-      this.offset = 100
+      this.totalData = Array.from({ length: 200 }, () => getRandomInt())
     }
   }
 }
